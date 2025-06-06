861a806188fb9908307ef9ec8239899a59b83cc0
apetbrz <apetbrz@gmail.com>
2025-06-06 19:21:26 -0400

    improved dev scripts

--- client/src/lib/Counter.svelte
+++ client/src/lib/Counter.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { greet } from "./pkg/portfolio_wasm"
+  import { greet } from "./pkg/portfolio"
   let count = $state(0)
   const increment = () => {
     count += 1
--- client/src/wasm-loader.js
+++
@@ -1,5 +0,0 @@
-export const loadWasm = async () => {
-	const wasm = await import('./wasm/portfolio_wasm.js');
-	await wasm.default();
-	return wasm;
-}
