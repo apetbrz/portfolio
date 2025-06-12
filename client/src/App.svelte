20f632bb6583a2b0dbc504eed9b642a1455df5b2
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-06-12 17:22:25 -0400

    added my links

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -12,9 +12,26 @@
         <h1 class="text-4xl font-extrabold">Arthur Petroff</h1>
     </div>
     <div
-        class="my-8 md:mx-8 p-8 bg-stone-700 md:rounded-lg justify-center drop-shadow-md shadow-stone-900/50 text-shadow-stone-900 text-shadow-md/50"
+        class="my-8 md:mx-8 p-8 bg-stone-700 md:rounded-lg justify-center drop-shadow-md shadow-stone-900/50 text-shadow-stone-900 text-shadow-md/50 flex space-y-2 flex-col"
     >
         <p>this website is still in development! check back later &lt;3</p>
+        <p>my links:</p>
+        <ul class="flex space-y-2 flex-col px-8">
+            <li>
+                <a
+                    href="https://github.com/apetbrz"
+                    class="underline hover:text-stone-400 max-w-min focus:outline-2 focus:outline-offset-2 rounded-xs"
+                    >github.com/apetbrz</a
+                >
+            </li>
+            <li>
+                <a
+                    href="https://www.linkedin.com/in/arthur-t-petroff/"
+                    class="underline hover:text-stone-400 max-w-min focus:outline-2 focus:outline-offset-2 rounded-xs"
+                    >linkedin.com/in/arthur-t-petroff</a
+                >
+            </li>
+        </ul>
     </div>
 
     <div class="p-4 mx-auto w-48">
