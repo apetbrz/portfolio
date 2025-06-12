1ed86b5b7643ece2eb8cc9d39b70dc7701954bf7
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-06-12 12:17:27 -0400

    added tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait(), svelte()],	
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
+    plugins: [
+        wasm(),
+        topLevelAwait(),
+        svelte(),
+        tailwindcss()
+    ],	
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+    },
 })
