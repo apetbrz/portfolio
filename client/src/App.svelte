68a4fc7ba1da092945299d0497774d850b119111
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-06-12 17:06:10 -0400

    added background image

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,24 +3,33 @@
 </script>
 
 <main
-    class="mx-auto overflow-hidden text-stone-200 font-mono lg:max-w-4xl md:max-w-2xl"
+    class="mx-auto overflow-hidden text-stone-200 font-mono lg:max-w-4xl md:max-w-2xl bg-scenic-image inset-shadow-sm/50"
 >
     <div
-        class="px-16 py-8 bg-stone-700 my-20 md:rounded-lg md:max-w-max mx-auto justify-center"
+        class="my-20 px-16 py-8 bg-stone-700 md:rounded-lg md:max-w-max mx-auto justify-center drop-shadow-md shadow-stone-900/50 text-shadow-stone-900 text-shadow-md/50"
     >
         <p class="text-sm italic">hi, i'm</p>
-        <h1 class="text-4xl font-extrabold">Arthur_Petroff</h1>
+        <h1 class="text-4xl font-extrabold">Arthur Petroff</h1>
     </div>
-
-    <div class="w-full p-8 border-y-2 border-stone-800">
+    <div
+        class="my-8 md:mx-8 p-8 bg-stone-700 md:rounded-lg justify-center drop-shadow-md shadow-stone-900/50 text-shadow-stone-900 text-shadow-md/50"
+    >
         <p>this website is still in development! check back later &lt;3</p>
     </div>
 
     <div class="p-4 mx-auto w-48">
         <Counter />
     </div>
+
+    <div class="h-[64rem]"></div>
 </main>
 
 <style lang="postcss">
     @reference "tailwindcss";
+    .bg-scenic-image {
+        background-image: url("assets/bg-scenic-image.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position-y: -24rem;
+    }
 </style>
