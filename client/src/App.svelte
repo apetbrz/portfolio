84fb7b29420aba666770d1befe14122df7f4b736
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-06-12 16:11:29 -0400

    switched to tailwindcss for page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,24 +1,26 @@
 <script>
-  import Counter from "./lib/Counter.svelte";
+    import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
-  <div class="header">
-    <p>hi, i'm</p>
-    <h1 class="name">Arthur Petroff</h1>
-  </div>
+<main
+    class="mx-auto overflow-hidden text-stone-200 font-mono lg:max-w-4xl md:max-w-2xl"
+>
+    <div
+        class="px-16 py-8 bg-stone-700 my-20 md:rounded-lg md:max-w-max mx-auto justify-center"
+    >
+        <p class="text-sm italic">hi, i'm</p>
+        <h1 class="text-4xl font-extrabold">Arthur_Petroff</h1>
+    </div>
 
-  <div class="info">
-    <p>this website is still in development! check back later</p>
-  </div>
+    <div class="w-full p-8 border-y-2 border-stone-800">
+        <p>this website is still in development! check back later &lt;3</p>
+    </div>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <div class="p-4 mx-auto w-48">
+        <Counter />
+    </div>
 </main>
 
-<style>
-  .header {
-    display: flex;
-  }
+<style lang="postcss">
+    @reference "tailwindcss";
 </style>
--- client/src/lib/Counter.svelte
+++ client/src/lib/Counter.svelte
@@ -1,12 +1,15 @@
 <script>
-  import { greet } from "./pkg/portfolio"
-  let count = $state(0)
-  const increment = () => {
-    count += 1
-    alert(greet())
-  }
+    import { greet } from "./pkg/portfolio";
+    let count = $state(0);
+    const increment = () => {
+        count += 1;
+        console.log(greet());
+    };
 </script>
 
-<button onclick={increment}>
-  count is {count}
+<button
+    class="p-2 bg-stone-700 rounded-md w-full border-stone-700 border-2 hover:bg-stone-800 active:bg-stone-900 focus:outline-2 focus:outline-stone-700 focus:outline-offset-2"
+    onclick={increment}
+>
+    count is {count}
 </button>
