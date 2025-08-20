ec212c2215d8821424c0bc078f6b33f699d57f0d
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-08-20 12:59:43 -0400

    refactored to clean up and simplify

--- client/src/lib/Marquee.svelte
+++ client/src/lib/Marquee.svelte
@@ -5,12 +5,12 @@
 <div class="pretty flex {props.indent ? '!pl-4' : ''}">
     <p class="!my-0">{props.static}</p>
     <p class="!m-0">&nbsp;</p>
-    <div class="overflow-hidden flex space-x-[1ch]">
-        <span class="titles text-nowrap">
-            {props.text}
+    <div class="overflow-hidden flex">
+        <span class="slide text-nowrap">
+            {props.text}&nbsp;
         </span>
-        <span class="titles text-nowrap">
-            {props.text}
+        <span class="slide text-nowrap">
+            {props.text}&nbsp;
         </span>
     </div>
 </div>
@@ -18,15 +18,15 @@
 <style lang="postcss">
     @reference "tailwindcss";
 
-    .titles {
-        animation: 30s slide-titles linear infinite 2s;
+    .slide {
+        animation: 30s slide linear infinite 2s;
     }
-    @keyframes slide-titles {
+    @keyframes slide {
         0% {
             transform: translateX(0);
         }
         100% {
-            transform: translateX(calc(-100% - 1ch));
+            transform: translateX(-100%);
         }
     }
 </style>
