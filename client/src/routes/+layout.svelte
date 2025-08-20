f828376878e9087ea0f3042b6dd3c5e97c037c08
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-08-20 11:29:08 -0400

    implemented custom fonts

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import "../app.css";
+    import "@fontsource-variable/eb-garamond";
+    import "@fontsource-variable/geist";
+    import "@fontsource-variable/fira-code";
 
     let { children } = $props();
 </script>
