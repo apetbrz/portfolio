b9f54742cfb7d273470d0795f3e00d39279df881
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-09-08 14:29:55 -0400

    demo wasm

---
+++ client/src/routes/projects/budget/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+    import NavBar from "$lib/NavBar.svelte";
+    import Demo from "./Demo.svelte";
+</script>
+
+<main
+    class="mx-auto overflow-hidden lg:max-w-4xl md:max-w-2xl bg-scenic-image md:inset-shadow-sm/50"
+>
+    <div class="pretty continent page-head">
+        <h1 class="!mb-0">Demos</h1>
+    </div>
+
+    <NavBar active="demos" />
+
+    <div class="m-8"></div>
+
+    <div class="pretty continent px-8 flex flex-col">
+        <h2>nclbt</h2>
+        <h4>nice command-line budget tool</h4>
+        <Demo />
+        <p class="indent-8">
+            Going through university teaches you a lot about yourself. I found
+            out that I am not as good with money as I'd like to be. To help with
+            this, I wrote a little budgeting tool to help me keep track of how
+            much I spend on things. It's got proper command-line support, as
+            well as a little interactive REPL mode, which is usable above.
+        </p>
+        <p class="indent-8">
+            It's written in pure Rust, and can be compiled as either a binary
+            for the command-line or as a library for use in other projects, such
+            as WASM for this demo. Hope you find it interesting!
+        </p>
+        <p class="indent-8">
+            Here's the commands for the interactive text interface: (everything
+            in [square brackets] is a value)
+        </p>
+        <ul>
+            <li>income set [amount]: sets your expected income</li>
+            <li>income raise [amount]: adds to your income</li>
+            <li>paid: receive your income</li>
+            <li>paid [amount]: receive some amount</li>
+            <li>
+                new [name] [amount]: create a new expenditure (overrides
+                existing copies)
+            </li>
+            <li>pay [name]: pay a static expenditure</li>
+            <li>pay [name] [amount]: pay some amount to an expenditure</li>
+            <li>save [amount]: add an amount into savings [unimpl.]</li>
+            <li>save all: add the remaining balance into savings [unimpl.]</li>
+        </ul>
+    </div>
+</main>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+    .bg-scenic-image {
+        background-image: url("$lib/assets/bg-scenic-image.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position-y: -24rem;
+    }
+</style>
---
+++ client/src/routes/projects/budget/Demo.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { Budget, format_dollars } from "$lib/pkg/nlbl.js";
+
+    const beautify = (acc) => {
+        let ce = acc.current_expenses.entries().toArray();
+        let ee = acc.expected_expenses.entries().toArray();
+
+        let exp = ce.map((e, i) => {
+            return `\n${e[0]}: ${format_dollars(e[1])} / ${format_dollars(ee[i][1])}`;
+        });
+
+        return `Welcome, ${acc.account}!\n
+                Current Balance: ${format_dollars(acc.current_balance)}
+                Expected Income: ${format_dollars(acc.expected_income)}
+                Expenses: ${exp}`;
+    };
+
+    let account = $state(new Budget("Demo User"));
+    let display = $derived(beautify(account.as_obj()));
+    let input = $state("");
+
+    const execute = () => {
+        console.log(input);
+        console.log(account);
+        console.log(account.__wbg_ptr);
+        try {
+            account = account.execute_string_immut(input);
+        } catch (err) {
+            console.error(err);
+        }
+        input = "";
+    };
+</script>
+
+<div class="flex flex-col gap-2 justify-center !m-8">
+    <div class="island !bg-stone-800 min-h-96 whitespace-pre-line">
+        {display}
+    </div>
+    <div class="flex gap-2 justify-center">
+        <input
+            type="text"
+            bind:value={input}
+            class="pretty bg-stone-800 md:w-[80%]"
+            onkeypress={(ev) => {
+                if (ev.key === "Enter") {
+                    execute();
+                }
+            }}
+        />
+        <button
+            type="submit"
+            class="form-input max-w-24 !bg-stone-800"
+            onclick={execute}
+        >
+            run
+        </button>
+    </div>
+</div>
