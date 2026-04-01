56446b43a029f162bf04dbafb009d5875653c2d1
Arthur Petroff Desktop <apetbrz@gmail.com>
2026-04-01 15:03:07 -0400

    hoverable title

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -5,7 +5,7 @@ let props = $props();
 <div class="pretty flex flex-row justify-center items-end">
 	<div class="w-min md:w-max page-head">
 		<p class="m-0! invisible md:visible">hi, I'm</p>
-		<a href="/" draggable="false" class="no-underline!">
+		<a href="/" draggable="false" class="pretty-button-nofocus no-underline!">
 			<h1 class="m-0!">
 				Arthur Taschetto
 			</h1>
