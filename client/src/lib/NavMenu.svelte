ab7ef00d156da44a2ed2f5f5d1564c1d81760f57
Arthur Petroff Desktop <apetbrz@gmail.com>
2026-04-01 14:15:29 -0400

    nav menu open fix

--- client/src/lib/NavMenu.svelte
+++ client/src/lib/NavMenu.svelte
@@ -16,7 +16,7 @@
     </a>
 {/snippet}
 
-<div on:click={() => open = true} on:mouseleave={() => open = false} class="absolute w-64 h-full transition-all duration-300 {open ? 'left-0':'-left-48'}">
+<div on:mouseleave={() => open = false} class="absolute w-64 h-full transition-all duration-300 z-10 {open ? 'left-0':'-left-48'}">
 	<button on:click={() => open = !open} class="pretty-button-nofocus absolute left-48 h-16 w-16">
 		<svg class="mx-auto" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
 			<line x1="2" x2="22" y1="6" y2="6" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1"></line>
