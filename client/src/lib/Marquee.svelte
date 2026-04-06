224d5c8fd2b715b9358193babd1141843d560e3a
Arthur Petroff <apetbrz@gmail.com>
2026-04-06 14:40:28 -0400

    navmenu improvements

--- client/src/lib/Marquee.svelte
+++ client/src/lib/Marquee.svelte
@@ -1,34 +1,34 @@
 <script>
-    let props = $props();
+let props = $props();
 </script>
 
 <div class="marquee pretty flex {props.indent ? 'pl-4!' : ''}">
-    <p class="my-0! text-nowrap">{props.static}</p>
-    <p class="m-0!">&nbsp;</p>
-    <div class="overflow-hidden">
-        <div class="w-max slide">
-            <span class="text-nowrap">
-                {props.text}
-            </span>
-            <span class="text-nowrap">
-                {props.text}
-            </span>
-        </div>
-    </div>
+	<p class="my-0! text-nowrap">{props.static}</p>
+	<p class="m-0!">&nbsp;</p>
+	<div class="overflow-hidden">
+		<div class="w-max slide">
+			<span class="text-nowrap">
+				{props.text}
+			</span>
+			<span class="text-nowrap">
+				{props.text}
+			</span>
+		</div>
+	</div>
 </div>
 
 <style lang="postcss">
-    @reference "tailwindcss";
+@reference "tailwindcss";
 
-    .slide {
-        animation: 20s slide linear infinite 2s;
-    }
-    @keyframes slide {
-        0% {
-            transform: translateX(0);
-        }
-        100% {
-            transform: translateX(-50%);
-        }
-    }
+.slide {
+	animation: 20s slide linear infinite 2s;
+}
+@keyframes slide {
+	0% {
+		transform: translateX(0);
+	}
+	100% {
+		transform: translateX(-50%);
+	}
+}
 </style>
--- client/src/lib/NavMenu.svelte
+++ client/src/lib/NavMenu.svelte
@@ -1,32 +1,101 @@
 <script>
-    import ThemeSelector from "$lib/ThemeSelector.svelte";
-    let props = $props();
-	let open = $state(false);
+import ThemeSelector from "$lib/ThemeSelector.svelte";
+let props = $props();
+let open = $state(false);
+
+$effect(() => {
+	let anim = open? "arrow-anim-open" : "arrow-anim-close";
+
+	let els = document.getElementsByClassName(anim)
+	console.table(els)
+	for(const el in els){
+		els.item(el)?.beginElement();
+	}
+})
+
+let tray_timeout_id;
+const tray_timeout_ms = 10_000
+const tray_icon_animation_dur = "0.2s"
+
+let handle_tray_toggle = () => {
+	reset_tray_timeout()
+	open = !open
+}
+let reset_tray_timeout = () => {
+	if(tray_timeout_id) clearTimeout(tray_timeout_id)
+}
+let handle_tray_timeout = () => {
+	tray_timeout_id = setTimeout(() => {open = false}, tray_timeout_ms)
+}
+
 </script>
 
 {#snippet link(display = "DEFAULT_FIXME", dest = "/" + display.toLowerCase())}
-    <a
-        href={dest}
-        class="pretty-button-nofocus h-16 content-center {props.active == display.toLowerCase()
-            ? 'underline'
-            : 'no-underline!'}"
-        draggable="false"
-    >
-        {display}
-    </a>
+	<a
+		href={dest}
+		class="pretty-button-nofocus px-8! h-16 content-center {props.active == display.toLowerCase()
+			? 'underline'
+			: 'no-underline!'}"
+		draggable="false"
+	>
+		{display}
+	</a>
 {/snippet}
 
-<div on:mouseleave={() => open = false} class="fixed w-64 top-0 bottom-0 min-h-[100vh] transition-all duration-300 z-10 {open ? 'left-0':'-left-48'}">
-	<button on:click={() => open = !open} class="pretty-button-nofocus absolute left-48 h-16 w-16">
+<div
+	on:mouseenter={reset_tray_timeout}
+	on:mouseleave={handle_tray_timeout}
+	on:focusout={(event) => {
+		if (
+			this.contains(event.relatedTarget) ||
+			!document.hasFocus()
+		) {
+			return;
+		}
+		open = false;
+	}}
+	class="fixed w-64 top-0 bottom-0 min-h-[100vh] transition-all duration-300 z-10 {open ? 'left-0':'-left-48'}"
+>
+	<button
+		on:click={handle_tray_toggle}
+		class="pretty-button-nofocus absolute left-48 h-16 w-16"
+	>
 		<svg class="mx-auto" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
-			<line x1="2" x2="22" y1="6" y2="6" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1"></line>
-			<line x1="2" x2="22" y1="12" y2="12" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1"></line>
-			<line x1="2" x2="22" y1="18" y2="18" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="1"></line>
+			<line x1="2" y1="6" x2="22" y2="6" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2">
+				<animate class="arrow-anim-open" attributeName="x1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="4" />
+				<animate class="arrow-anim-open" attributeName="x2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="20" />
+				<animate class="arrow-anim-close" attributeName="x1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="2" />
+				<animate class="arrow-anim-close" attributeName="x2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="22" />
+
+				<animate class="arrow-anim-open" attributeName="y1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="4" />
+				<animate class="arrow-anim-close" attributeName="y1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="6" />
+				<animate class="arrow-anim-open" attributeName="y2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="20" />
+				<animate class="arrow-anim-close" attributeName="y2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="6" />
+			</line>
+			<line x1="2" y1="12" x2="22" y2="12" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2">
+				<animate class="arrow-anim-open" attributeName="x1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="12" />
+				<animate class="arrow-anim-open" attributeName="x2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="12" />
+				<animate class="arrow-anim-close" attributeName="x1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="2" />
+				<animate class="arrow-anim-close" attributeName="x2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="22" />
+			</line>
+			<line x1="2" y1="18" x2="22" y2="18" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2">
+				<animate class="arrow-anim-open" attributeName="x1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="4" />
+				<animate class="arrow-anim-open" attributeName="x2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="20" />
+				<animate class="arrow-anim-close" attributeName="x1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="2" />
+				<animate class="arrow-anim-close" attributeName="x2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="22" />
+
+				<animate class="arrow-anim-open" attributeName="y1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="20" />
+				<animate class="arrow-anim-close" attributeName="y1" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="18" />
+				<animate class="arrow-anim-open" attributeName="y2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="4" />
+				<animate class="arrow-anim-close" attributeName="y2" dur={tray_icon_animation_dur} fill="freeze" begin="indefinite" to="18" />
+			</line>
 		</svg>
 	</button>
-	<div class="pretty sans flex flex-col bg-grv-bg3 h-full relative transition-all duration-300 w-48">
+
+	<div class="pretty sans flex flex-col bg-grv-bg3 h-full relative transition-all duration-300 w-48" tabindex="-1" inert={!open}>
 		{@render link("Home", "/")}
 		{@render link("Projects")}
+		{@render link("Blog")}
 		<ThemeSelector />
 	</div>
 </div>
