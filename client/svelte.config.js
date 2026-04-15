81797d4b1aa105bf6251e504ff04a1c724b31adb
Arthur Petroff <apetbrz@gmail.com>
2026-04-15 13:52:29 -0400

    fixed server prerender being off (FOUC)

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,16 +1,14 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    kit: {
         adapter: adapter({
             pages: '../dist',
             assets: '../dist',
             fallback: 'index.html',
-            precompress: false,
+            precompress: true,
             strict: true
         })
     },
