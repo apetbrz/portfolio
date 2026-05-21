8fea5ccf415d3c86a07f4cf1afd9f60c2299c8b2
Arthur Petroff <apetbrz@gmail.com>
2026-05-21 13:11:03 -0400

    metadata support

--- client/src/routes/blog/+layout.js
+++ client/src/routes/blog/+layout.js
@@ -4,13 +4,13 @@ import { error } from '@sveltejs/kit';
 // export const csr = true;
 export const ssr = false;
 
-export const load = async ({ fetch, params }) => {
-	const res = await fetch(`/blog/content/${params.slug}`)
-	const html = await res.text()
+export const load = async ({ fetch }) => {
+	const res = await fetch(`/blog/metadata`)
+	const data = await res.json()
 
-	if (!html) {
+	if (!data) {
 		error(404, "Can't find content");
 	}
 
-	return { html };
+	return { entries: data };
 }
--- server/src/cfg.rs
+++ server/src/cfg.rs
@@ -1,11 +1,13 @@
+use reqwest::Url;
+
 #[derive(Clone)]
 pub struct ServerConfig {
-    pub markdown_cache_server: String
+    pub markdown_cache_server: Url
 }
 impl ServerConfig {
     pub fn new(md_server: String) -> Self {
         ServerConfig {
-            markdown_cache_server: md_server
+            markdown_cache_server: Url::parse(&md_server).unwrap()
         }
     }
 }
