d0c9de2e5cc579ecb68258b55db392897137f830
Arthur Petroff <apetbrz@gmail.com>
2026-05-29 16:35:25 -0400

    polished blog pages

--- client/src/routes/blog/+page.svelte
+++ client/src/routes/blog/+page.svelte
@@ -8,21 +8,21 @@ let { data } = $props();
 
 {#snippet post(entry)}
 <li class="w-full py-4">
-	<a href={"/blog/" + entry[0]} class="no-underline! font-normal! hover:bg-grv-bg2 focus:bg-grv-bg2 focus:outline-none outline-none">
-		<div class="hover:bg-grv-bg2 focus:bg-grv-bg2">
+	<a href={"/blog/" + entry[0]} class="no-underline! font-normal! focus:outline-none outline-none group">
+		<div class="p-4 group-hocus:bg-grv-bg2">
 			<span>
-				<span class="pretty-link">
+				<span class="pretty-link group-hocus:font-bold group-hocus:text-grv-green-bright">
 					{entry[1].title}
 				</span>
 				- {Temporal.PlainDate.from(entry[1].date).toLocaleString()}
 			</span>
-			<p>{entry[1].description}</p>
+			<p class="mb-0!">{entry[1].description}</p>
 		</div>
 	</a>
 </li>
 {/snippet}
 
-<ul id="maindiv" class="flex flex-col justify-center items-center mx-auto w-md pretty divide-y divide-grv-gray-faded">
+<ul id="maindiv" class="flex flex-col mx-auto pretty divide-y divide-grv-gray-faded w-lg">
 	{#if data.error}
 		<h3>{data.message}</h3>
 		<p>{data.error}</p>
--- client/src/routes/blog/[slug]/+page.svelte
+++ client/src/routes/blog/[slug]/+page.svelte
@@ -1,28 +1,31 @@
 <script>
 let { params, data } = $props();
 
-let post_meta = data.entries?.find((element) => element[0] === params.slug)[1]
+let post_meta = $derived(data.entries?.find((element) => element[0] === params.slug)[1]);
 </script>
 
 <svelte:head>
-	<title>{post_meta.title} - Arthur's Blog</title>
+	<title>{post_meta?.title} - Arthur's Blog</title>
 </svelte:head>
 
-<div id="maindiv" class="text-left w-full md:w-lg lg:w-4xl md:mx-auto pretty" >
+<div id="maindiv" class="text-left w-full md:w-2xl lg:w-4xl md:mx-auto pretty" >
+{#if post_meta}
 	<div class="pretty sans text-center w-full my-16">
-		{#if data.error}
-			<h1>Oops!</h1>
-		{:else}
-			<h1>{ post_meta?.title }</h1>
-		{/if}
+	{#if data.error}
+		<h1>Oops!</h1>
+	{:else}
+		<h1>{ post_meta?.title }</h1>
+	{/if}
 	</div>
 
-	{#if data.error}
-		<h3>{data.message}</h3>
-		<p>{data.error}</p>
-	{:else}
-		{@html data.html }
-	{/if}
+{#if data.error}
+	<h3>{data.message}</h3>
+	<p>{data.error}</p>
+{:else}
+	{@html data.html }
+{/if}
+
+{/if}
 </div>
 
 <style lang="postcss">
